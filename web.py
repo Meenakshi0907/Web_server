52d9931b1bcd5725aee7e305f5fb2e1a447cabe8
Meenakshi0907 <meenakshiapril25@gmail.com>
2021-11-30 20:42:52 +0530

    ch

---
+++ web.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>Meenakshi M</h1>
+<h1>21003572</h1>
+<h1>ARTIFICIAL INTELLGENCE AND DATASCIENCE</h1>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8080)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
